c5225f2e46f4988f9052ee90de083e897c74cc1e
Clinton Beasley <cbeasley92@gmail.com>
2016-04-16 19:59:17 -0500

    Moving models.py to rest_api.py

--- officelunch_api/rest_api.py
+++ officelunch_api/rest_api.py
@@ -7,6 +7,7 @@ from flask import Flask, jsonify, abort, make_response, request, url_for
 from flask_cors import CORS
 from flask_mongoengine import MongoEngine
 
+import datetime
 
 app = Flask(__name__)
 CORS(app, resources=r'/api/*', headers='Content-Type')
@@ -15,8 +16,28 @@ app.config['MONGODB_SETTINGS'] = {'db': 'officelunch'}
 
 db = MongoEngine(app)
 
-from models import User
-from models import Lgroup
+
+class User(db.Document):
+    name = db.StringField(max_length=255, required=True)
+    email = db.EmailField(required=True)
+    password = db.StringField(max_length=255, required=True)
+    phone = db.StringField() #Does this need to be required? This will need to be checked somewhere else in the application since there is no mongo field for phone numbers
+    userid = db.StringField(max_length=255, required=True)
+    member_of = db.ListField(db.StringField()) #list of lgids that the user belongs to
+
+
+class Lgroup(db.Document):
+    name = db.StringField(max_length=255, required=True)
+    lgid = db.StringField(required=True)
+    users = db.ListField(db.DictField(), required=True)
+    origin = db.DictField(required=True) #This will be a python dictionary storing lat/long
+    tags = db.ListField(db.StringField(max_length=255), required=True)
+    dest_list = db.ListField(db.DictField()) #This is a list of dictionaries holding lat/long and # of votes
+    final_dest = db.DictField() #dictionary with lat/long of voting winner
+    admin = db.StringField(max_length=255, required=True) #id of the group admin
+    start_time = db.DateTimeField(default=datetime.datetime.now, required=True)
+    end_time = db.DateTimeField()#end date of the event
+
 
 users = [
     {
